aff4738991b78994281c56415fcbc8ad3a22073d
ITsLoveCats <67352969+ITsLoveCats@users.noreply.github.com>
2022-05-15 13:15:50 +0800

    Update plugins.lua

--- lua/ns/plugins.lua
+++ lua/ns/plugins.lua
@@ -1,1 +1,59 @@
+function get_setup(name)
+  return string.format('require("setup/%s")', name)
+end
 
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- tree-sitter
+  use 'nvim-treesitter/nvim-treesitter'
+
+  -- nvim web devicons
+  use 'kyazdani42/nvim-web-devicons'
+
+  -- lualine
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+ }
+
+   -- A File Explorer
+  use {
+      'kyazdani42/nvim-tree.lua',
+      requires = {
+        'kyazdani42/nvim-web-devicons', -- optional, for file icon
+      },
+      config = function() require('nvim-tree').setup({}) end
+  }
+
+  -- Autopair Plugin
+  use {
+    'windwp/nvim-autopairs',
+    config = function() require('nvim-autopairs').setup() end
+  }
+
+  -- Collection of configurations for the built-in LSP client
+  use {
+    'neovim/nvim-lspconfig',
+  }
+
+  -- rust rust-tools
+  use { 'simrat39/rust-tools.nvim',
+    config = function()
+        require('rust-tools').setup({})
+    end
+  }
+
+  -- nvim completion
+  use {'hrsh7th/cmp-nvim-lsp'}
+  use {'hrsh7th/cmp-buffer'}
+  use {'hrsh7th/cmp-path'}
+  use {'hrsh7th/cmp-cmdline'}
+  use {'L3MON4D3/LuaSnip'}
+  use {'saadparwaiz1/cmp_luasnip'}
+  use {
+      'hrsh7th/nvim-cmp',
+      config = get_setup("nvim-cmp"),
+  }
+end)
