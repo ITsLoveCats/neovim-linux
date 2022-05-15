036bd72a948f9935c91bdc8e21c6d20ecc25b8a0
ITsLoveCats <67352969+ITsLoveCats@users.noreply.github.com>
2022-05-15 12:58:34 +0800

    Update config.lua

--- lua/ns/config.lua
+++ lua/ns/config.lua
@@ -1,1 +1,17 @@
+vim.o.number = true
+vim.o.relativenumber = true
 
+-- tab vs space
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.opt.foldmethod = "syntax"
+vim.opt.foldlevelstart = 999
+vim.o.expandtab = true
+
+-- split habit
+vim.o.splitbelow = true
+vim.o.splitright = true
+
+-- colorscheme
+--vim.api.nvim_command('colorscheme tokyonight')
