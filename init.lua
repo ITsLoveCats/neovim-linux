23d3096d89a491e901ca7e3bc2de3b168bfcf413
ITsLoveCats <67352969+ITsLoveCats@users.noreply.github.com>
2022-05-15 14:12:34 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
-require("plugins")
-require("options")
-require("mappings")
+-- https://gist.github.com/benfrain/97f2b91087121b2d4ba0dcc4202d252f
+require("ns/plugins")
+require("ns/options")
+require("ns/mappings")
