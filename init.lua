2c73525a3e25b4297f1a91aa1bce80c1dd9e985e
ITsLoveCats <67352969+ITsLoveCats@users.noreply.github.com>
2022-05-15 14:45:25 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,11 @@
 -- https://gist.github.com/benfrain/97f2b91087121b2d4ba0dcc4202d252f
+
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
 require("ns/plugins")
-require("ns/options")
-require("ns/mappings")
+require("ns/config")
+require("ns/keybinding")
